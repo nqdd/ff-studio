1697e7f313e6481faef06c349f84d17b3cc2fbf2
nqdd <datnq1210@gmail.com>
2025-10-26 10:59:00 +0700

    fix(test): import path

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,6 @@
+import App from '@/renderer/main';
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
-import App from '../renderer/App';
 
 describe('App', () => {
   it('should render', () => {

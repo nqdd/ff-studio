d688d7f7c978e6a60403281e40136c33ba4ae18e
nqdd <datnq1210@gmail.com>
2025-10-26 22:59:21 +0700

    refactor: remove some UI

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,22 +1,12 @@
 import './styles/global.css';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import { FileUpload } from './components/file-upload';
-import { ConversionQueue } from './components/conversion-queue';
-import { ConversionSettings } from './components/conversion-settings';
 
 function Hello() {
   return (
     <main className="min-h-screen bg-background">
       <div className="container mx-auto px-4 py-8 max-w-6xl">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-          <div className="lg:col-span-2 space-y-6">
-            <FileUpload />
-            <ConversionSettings />
-          </div>
-          <div className="lg:col-span-1">
-            <ConversionQueue />
-          </div>
-        </div>
+        <FileUpload />
       </div>
     </main>
   );
